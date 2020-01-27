49e92c085e3b8a3a548b86728e7999d7081c4a30
Protimus <rafaelgorges@outlook.com>
2020-01-27 14:42:28 -0300

    Added example of REST endpoint to do a CRUD

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Got a GET request at /users');
 });
+
+/* POST user. */
+router.post('/', function (req, res) {
+  res.send('Got a POST request at /users');
+});
+
+/* PUT user. */
+router.put('/', function (req, res) {
+  res.send('Got a PUT request at /users');
+});
+
+/* DELETE user. */
+router.delete('/', function (req, res) {
+  res.send('Got a DELETE request at /users');
+});
+
 
 module.exports = router;
