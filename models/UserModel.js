76bdd90aacf073bd68e2379728d2579df7e99161
Protimus <rafaelgorges@outlook.com>
2020-01-27 17:05:59 -0300

    Added mongojs and refactoring for users model to use mongo, created default configurator with mongoDB variables to connection

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,7 +1,31 @@
 class UserModel {
-    find(query, callback){};
-    create(data, callback){};
-    update(_id, data, callback){};
+
+    UserModel(mongo){
+        this.mongo = mongo;
+    }
+    
+    find(query, callback){
+        this.mongo.collection('users').find(query, callback);
+    };
+
+    findOne(_id, callback){
+        var query = { _id: this.mongo.ObjectId(_id) };
+        this.mongo.collection('users').findOne(query, callback);
+    };
+
+    create(data, callback){
+        this.mongo.collection('users').insert(data, callback);
+    }
+
+    update(_id, callback){
+        var query = { _id: this.mongo.ObjectId(_id) };
+        this.mongo.collection('users').insert(query, callback);
+    };
+
+    remove(_id, callback){
+        var query = { _id: this.mongo.ObjectId(_id) };
+        this.mongo.collection('users').remove(query, callback);
+    }
 }
 
-module.exports = new UserModel();
+module.exports = new UserModel(mongo);
