bb8af651ae1e2d476b6dd2c7592296e4c781222d
Protimus <rafaelgorges@outlook.com>
2020-01-27 17:23:25 -0300

    Refactoring users controller to use values of model and sanitized possible errors when perform the requests

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,23 +1,65 @@
+var debug = require('debug')('api:controller');
+
 class UserController {
+
+    UserController(UserModel){
+        this.model = UserModel;
+    };
+
     getAll(request, response, next){
-        response.send('get all users');
-    }
+        this.model.find({},function(err, data){ 
+            if(err){
+                return next(err);
+            } 
+            response.json(data);
+            response.send('You have get all users');
+        });
+    };
 
     getById(request, response, next){
-        response.send('get specific user by id');
-    }
+        var _id = request.params._id;
+        this.model.findOne(_id, function(err, data){
+            if(err){
+                return next(err);
+            }
+            response.json(data);
+            response.send('You have get the user with ID: ' + _id);
+        });
+    };
     
     create(request, response, next){
-        response.send('create a new user');
-    }
+        var body = request.body;
+        this.model.create(body, function(err, data){
+            if(err){
+                return next(err);
+            }
+            response.json(data);
+            response.send('You have created a new user');
+        })
+    };
     
     update(request, response, next){
-        response.send('edit a specific user');
-    }
+        var _id = request.params._id,
+        body = request.body;
+        this.model.update(_id, body, function(err, data){
+            if(err){
+                return next(err);
+            }
+            response.json(data);
+            response.send('You have updated the user with ID: ' + _id);
+        })
+    };
     
     remove(request, response, next){
-        response.send('delete a specific user');
-    }
-}
+        var _id = request.params._id;
+        this.model.remove(_id, function(err, data){
+            if(err){
+                return next(err);
+            }
+            response.json(data);
+            response.send('You have deleted the user with ID: ' + _id);
+        });
+    };
+};
 
 module.exports = new UserController();
